362fefaf8f9fee2901fc9f6efe383601ca55ad01
amrou <amrou>
2024-11-11 12:56:41 +0100

    lezione del 11/11

--- spotify/src/app/app.component.ts
+++ spotify/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { SpotifyService } from './spotify.service';
 
 
 @Component({
@@ -9,6 +10,18 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  //questo sinal consente al componente di sapere se è presente il token bearer dell'applicazione
+  validToke: Signal<boolean>
+
+  constructor(private spotifyServices: SpotifyService) {
+    this.validToke = this.spotifyServices.tokenValid;
+  }
+
+  //il metodo ngOnInit viene eseguito poco prima che il COMPONENTE sia disponibile per l'utente,
+  //cioè prima che il componente venga eseguito il reandere da parte del browser
+  ngOnInit(): void {
+    this.spotifyServices.getToken();
+  }
 
 }
